f6c42ae99dc77662570c05908c4a1ebaf902e5bf
liuxuhelloworld <liuxuhelloworld@126.com>
2019-12-04 08:06:34 +0800

    proxy v2

---
+++ proxy/v2/MyRemoteImpl.java
@@ -0,0 +1,24 @@
+package proxy.v2;
+
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class MyRemoteImpl extends UnicastRemoteObject implements MyRemote {
+
+    public MyRemoteImpl() throws RemoteException {
+    }
+
+    public String sayHello() {
+        return "Server says, 'hey'";
+    }
+
+    public static void main(String[] args) {
+        try {
+            MyRemote service = new MyRemoteImpl();
+            Naming.rebind("RemoteHello", service);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
