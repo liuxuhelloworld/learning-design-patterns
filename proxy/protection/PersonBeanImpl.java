8d1cbe5ce0fd18d19a58d837131159b1cc9ae9f2
liuxuhelloworld <liuxuhelloworld@126.com>
2019-12-09 08:06:51 +0800

    protection proxy pattern

---
+++ proxy/protection/PersonBeanImpl.java
@@ -0,0 +1,53 @@
+package proxy.protection;
+
+public class PersonBeanImpl implements PersonBean {
+    private String name;
+    private String gender;
+    private String interests;
+    private int rating = 0;
+    private int ratingCount = 0;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getGender() {
+        return gender;
+    }
+
+    @Override
+    public String getInterests() {
+        return interests;
+    }
+
+    @Override
+    public int getHotOrNotRating() {
+        if (ratingCount == 0) {
+            return 0;
+        } else {
+            return rating / ratingCount;
+        }
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public void setInterests(String interests) {
+        this.interests = interests;
+    }
+
+    public void setHotOrNotRating(int rating) {
+        this.rating += rating;
+        ratingCount++;
+    }
+}
