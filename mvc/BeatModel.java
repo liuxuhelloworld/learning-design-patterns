0485d01b45cfbe19038ea48c28077feb794b054c
liuxuhelloworld <liuxuhelloworld@126.com>
2019-12-14 07:23:30 +0800

    mvc pattern

---
+++ mvc/BeatModel.java
@@ -0,0 +1,95 @@
+package mvc;
+
+import javax.sound.midi.*;
+import java.util.ArrayList;
+
+public class BeatModel implements BeatModelInterface, MetaEventListener {
+    private ArrayList<BeatObserver> beatObservers = new ArrayList<>();
+    private ArrayList<BPMObserver> bpmObservers = new ArrayList<>();
+    private int bpm = 90;
+    private Sequencer sequencer;
+    private Sequence sequence;
+    private Track track;
+
+    @Override
+    public void initialize() {
+        setUpMidi();
+        buildTrackAndStart();
+    }
+
+    @Override
+    public void on() {
+        sequencer.start();
+        setBPM(90);
+    }
+
+    @Override
+    public void off() {
+        setBPM(0);
+        sequencer.stop();
+    }
+
+    @Override
+    public void setBPM(int bpm) {
+        this.bpm = bpm;
+        sequencer.setTempoInBPM(getBPM());
+        notifyBPMObservers();
+    }
+
+    private void notifyBPMObservers() {
+        bpmObservers.stream().forEach(BPMObserver::updateBPM);
+    }
+
+    private void setUpMidi() {
+        try {
+            sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+            sequencer.addMetaEventListener(this);
+            sequence = new Sequence(Sequence.PPQ, 4);
+            track = sequence.createTrack();
+            sequencer.setTempoInBPM(getBPM());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void buildTrackAndStart() {
+        int[] trackList = {35, 0, 46, 0};
+
+        sequence.deleteTrack(null);
+        track = sequence.createTrack();
+
+        makeTracks(trackList);
+        track.add(makeEvent(192,9,1,0,4));
+        try {
+            sequencer.setSequence(sequence);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void makeTracks(int[] list) {
+
+        for (int i = 0; i < list.length; i++) {
+            int key = list[i];
+
+            if (key != 0) {
+                track.add(makeEvent(144,9,key, 100, i));
+                track.add(makeEvent(128,9,key, 100, i+1));
+            }
+        }
+    }
+
+    public  MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
+        MidiEvent event = null;
+        try {
+            ShortMessage a = new ShortMessage();
+            a.setMessage(comd, chan, one, two);
+            event = new MidiEvent(a, tick);
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return event;
+    }
+}
